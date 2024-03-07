214f592be070afa32a3e428862f1857a5eacacfe
Hmazah <alhamza2012@gmail.com>
2024-03-07 10:00:00 +0200

    add mobile navgation

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@
 import './App.css'
 import HeroSlider from './components/hero/HeroSlider'
 import React, { StrictMode } from 'react';
-import Navbar from './components/ui/Navbar';
+
 import CardSection from './components/cardsecsion/cardsection';
 const App = () => {
   return (
    
    <>
-
-   <h1 className='text-white text-center text-4xl font-bold'>Hello</h1>
+   
+   <h1 className='text-white text-center text-4xl font-bold  backdrop-blur-md '>Hello</h1>
    
      <div className="flex flex-col p-10  gap-10 items-start justify-start bg-red-600 text-white relative  h-screen  w-screen overflow-hidden">
      
