17538347abbd837af23355b346da5ad12191b4b7
Hmazah <alhamza2012@gmail.com>
2024-03-08 10:00:00 +0200

    add mobile navgation

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,21 @@ import './App.css'
 import HeroSlider from './components/hero/HeroSlider'
 import React, { StrictMode } from 'react';
 
-import CardSection from './components/cardsecsion/cardsection';
+import CardSection from './components/cardsecsion ';
 const App = () => {
   return (
    
    <>
    
-   <h1 className='text-white text-center text-4xl font-bold  backdrop-blur-md '>Hello</h1>
+   <h1 className='text-white text-center text-4xl font-bold  bounce '>Hello</h1>
    
-     <div className="flex flex-col p-10  gap-10 items-start justify-start bg-red-600 text-white relative  h-screen  w-screen overflow-hidden">
+     <div className="flex flex-col p-10  gap-10 items-start justify-start bg-red-600 text-white relative  h-auto  w-screen ">
      
        <HeroSlider />
+       </div>
       <CardSection />
 
-      </div>
+     
     </>
   )
 }
--- src/components/cardsecsion/cardsection.jsx
+++ src/components/cardsecsion/cardsection.jsx
@@ -3,9 +3,10 @@ import Card from "../ui/Card";
 
 const CardSection = () => {
       {/* Cards Section */}
-      <div className="container mx-auto px-4 mt-16">
+
+      <div className=" px-4 mt-16">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <Card className="bg-white text-gray-800 p-6 rounded-lg shadow-lg hover:shadow-xl transition">
+          <Card  variant='outlined' className="bg-white text-gray-800 p-6 rounded-lg shadow-lg hover:shadow-xl transition">
             <div className="text-center">
               <div className="text-4xl text-red-600 mb-4">🍕</div>
               <h3 className="text-xl font-bold mb-2">Świeże Składniki</h3>
@@ -13,7 +14,7 @@ const CardSection = () => {
             </div>
           </Card>
 
-          <Card className="bg-white text-gray-800 p-6 rounded-lg shadow-lg hover:shadow-xl transition">
+          <Card  variant='outlined' className="bg-white text-gray-800 p-6 rounded-lg shadow-lg hover:shadow-xl transition">
             <div className="text-center">
               <div className="text-4xl text-red-600 mb-4">🚚</div>
               <h3 className="text-xl font-bold mb-2">Szybka Dostawa</h3>
@@ -21,7 +22,7 @@ const CardSection = () => {
             </div>
           </Card>
 
-          <Card className="bg-white text-gray-800 p-6 rounded-lg shadow-lg hover:shadow-xl transition">
+          <Card  variant='outlined' className="bg-white text-gray-800 p-6 rounded-lg shadow-lg hover:shadow-xl transition">
             <div className="text-center bg-amber-100">
               <div className="text-4xl text-red-600 mb-4">⭐</div>
               <h3 className="text-xl font-bold mb-2">Najlepsza Jakość</h3>
